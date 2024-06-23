091d5fce8299fab0d802c0eaf78f48f9cda507a5
Almuhannad Alotaibi <almuhannad.khalid@gmail.com>
2024-06-23 16:03:23 +0300

    practiced operators on streams like map, and filter, and some basic unit tests

--- reactive-programming-using-reactor/src/main/java/com/learnreactiveprogramming/service/FluxAndMonoGeneratorService.java
+++ reactive-programming-using-reactor/src/main/java/com/learnreactiveprogramming/service/FluxAndMonoGeneratorService.java
@@ -12,14 +12,53 @@ public class FluxAndMonoGeneratorService {
     // Flux
     public Flux<String> namesFlux() {
 
-        return Flux.fromIterable(List.of("Ahmad", "Bader", "Caeser")); // might come from a db or a remote service call
+        return Flux.fromIterable(List.of("Alex", "Ben", "Caeser")) // might come from a db or a remote service call
+                .log(); // log to show the flow of events like request(), onNext(), onComplete() like diagrams
 
     }
+
+    // using map operator to apply transformations
+    public Flux<String> namesFluxMap() {
+        return Flux.fromIterable(List.of("ibrahim", "mohammad", "ismael", "mousa")).map(name ->
+                name.toUpperCase()).log();
+    }
+
+    // reactive streams are immutable (taking from namesFlux, storing, then trying to change values of stream)
+    public Flux<String> namesFluxImmutable() {
+
+        var names = Flux.fromIterable(List.of("aaa", "bbb", "ccc"));
+
+        // if i return here, the stream will be transformed to upper case
+        names.map(name ->
+                name.toUpperCase());
+
+        // if i return here, nothing will happen to the stream as changes will be dropped
+        // and no transformations will be applied (immutable streams)
+        return names;
+    }
+
+
+    // filter operator used to filter elements of stream based on condition (predicate)
+    public Flux<String> namesFluxFilter(int elementLength) {
+
+        var filteredNames = Flux.fromIterable(List.of("ed", "edd", "eddy", "johnny", "jack", "jameson"));
+
+        // combining filter with map to get a better result
+        // chaining multiple operators is called (pipeline) in reactive streams
+        return filteredNames.filter(name ->
+                name.length() > elementLength)
+                .map(name ->
+                        name.length() + "-" + name);
+    }
+
+
 
     //Mono
     public Mono<String> nameMono() {
-        return Mono.just("Almuhannad");
+        return Mono.just("Almuhannad").log();
     }
+
+
 
     public static void main(String[] args) {
 
@@ -34,7 +73,22 @@ public class FluxAndMonoGeneratorService {
                 .subscribe(name ->
                         System.out.println("Mono name is: "+ name));
 
+        fluxAndMonoGeneratorService.namesFluxMap()
+                .subscribe(name ->
+                        System.out.println("Capitalized name is: " + name));
+
+
+        fluxAndMonoGeneratorService.namesFluxImmutable()
+                .subscribe(name ->
+                        System.out.println("Immutable name is: " + name));
+
+        fluxAndMonoGeneratorService.namesFluxFilter(3)
+                .subscribe(name ->
+                        System.out.println("Filtered name is: " + name));
+
+
     }
+
 
 
 
--- reactive-programming-using-reactor/src/test/java/com/learnreactiveprogramming/service/FluxAndMonoGeneratorServiceTest.java
+++ reactive-programming-using-reactor/src/test/java/com/learnreactiveprogramming/service/FluxAndMonoGeneratorServiceTest.java
@@ -1,12 +1,64 @@
 package com.learnreactiveprogramming.service;
 
 
+import org.junit.jupiter.api.Test;
+import reactor.test.StepVerifier;
 
 class FluxAndMonoGeneratorServiceTest {
+
+
+    // REACTOR UNIT TESTING
 
     FluxAndMonoGeneratorService fluxAndMonoGeneratorService = new FluxAndMonoGeneratorService();
 
 
+    @Test
+    void namesFlux() {
+        //given
+
+        //when
+        var namesFlux = fluxAndMonoGeneratorService.namesFlux();
+
+        //then (reactor test)
+        StepVerifier.create(namesFlux)
+                //.expectNext("Ahmad", "Bader", "Caeser")
+                //.expectNextCount(3)
+                .expectNext("Ahmad")
+                .expectNextCount(2)
+                .verifyComplete();
+
+    }
+
+    @Test
+    void namesFluxMap() {
+
+        var namesFluxMap = fluxAndMonoGeneratorService.namesFluxMap();
+
+        StepVerifier.create(namesFluxMap)
+                .expectNext("IBRAHIM", "MOHAMMAD", "ISMAEL", "MOUSA")
+                //.expectNextCount(4)
+                .verifyComplete();
+    }
+
+    @Test
+    void namesFluxImmutable() {
+
+        var namesFluxImmutable = fluxAndMonoGeneratorService.namesFluxImmutable();
+
+        StepVerifier.create(namesFluxImmutable)
+                .expectNext("aaa", "bbb", "ccc")
+                //.expectNextCount(3)
+                .verifyComplete();
+    }
 
 
+    @Test
+    void namesFluxFilter() {
+
+        var namesFluxFilter = fluxAndMonoGeneratorService.namesFluxFilter(3);
+
+        StepVerifier.create(namesFluxFilter)
+                .expectNext("4-eddy", "6-johnny", "4-jack", "7-jameson")
+                .verifyComplete();
+    }
 }
